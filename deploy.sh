5bbf9e12ef18e02295a783c4a7f96c128f26052f
Assaf Cohen <ascohen@trustwave.com>
2019-06-16 12:05:24 +0300

    get all source patch it and build it

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,29 @@
-#!/bin/bash
-
+#!/bin/bash -x
+ROOT_DIR=${PWD}
+SAMBA_DIR=${ROOT_DIR}/samba
 #get samba
 git clone https://github.com/samba-team/samba.git samba
+cd ${SAMBA_DIR}
 #patch samba
-patch -p1 samba.patch
+patch -p1  -i ${ROOT_DIR}/samba.patch
 #put trustwave dir
-cp -r trustwave samba/ 
+cp -r ${ROOT_DIR}/trustwave ${SAMBA_DIR}
 #get pev
-https://github.com/merces/pev.git samba/trustwave/3rdparty/ 
+git clone --recursive https://github.com/merces/pev.git trustwave/3rdparty/pev
 #patch pev
-patch -p1 pev.patch
+cd trustwave/3rdparty/pev
+patch -p1 -i ${ROOT_DIR}/pev.patch
+cd lib/libpe
+patch -p1 -i ${ROOT_DIR}/libpe.patch
+cd ../..
 #build pev
-cd samba/trustwave/3rdparty/pev
 make
 #get json
-https://github.com/taocpp/json.git samba/trustwave/3rdparty/
+git clone https://github.com/taocpp/json.git trustwave/3rdparty/json
 #configure
+cd ${SAMBA_DIR}
+./buildtools/bin/waf configure  --without-ad-dc --without-json --without-ldap --without-ldap --enable-developer --without-ads  --without-acl-support --disable-python --boost-libs=/usr/lib
 #build
+./buildtools/bin/waf build
 
 
