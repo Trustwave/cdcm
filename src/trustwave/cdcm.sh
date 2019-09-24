0c0930ed2ab98c2823e815430e1c4c7e2dee722d
Assaf Cohen <ascohen@trustwave.com>
2019-09-24 18:02:49 +0300

    Removed rpath Added another install location Added run scripts

---
+++ src/trustwave/cdcm.sh
@@ -0,0 +1,78 @@
+!/bin/sh
+ulimit -c 0
+ulimit -n 131072
+TODO=start
+RUN_USER=root
+RUN_GROUP=root
+CDCM_EXE=cdcm_broker
+CDCM_LOG_DIR=/var/cdcm/log
+G_TMP=/var/cdcm/run
+START_CMD="exec start-stop-daemon --start -c $RUN_USER -g $RUN_GROUP -k 0002 -d $G_TMP"
+STOP_CMD="start-stop-daemon --stop --oknodo --retry=TERM/10/KILL/5"
+KILL_CMD="start-stop-daemon --stop --oknodo --signal KILL --retry 5"
+STATUS_CMD="start-stop-daemon --status"
+
+# Common functions
+usage(){
+    echo -e "\nUsage: $(basename $0) [--help|start|stop|status]\n"
+    exit 0
+}
+
+PID="-1"
+BASENAME=`basename $0`
+while [ $# -gt 0 ];do
+    case $1 in
+        --help|-h)  usage;;
+        start|stop|status) TODO=$1;;
+        --pid)      shift; PID=$1;;
+        *) echo "Unknow argument $1";usage;;
+    esac
+    shift
+done
+
+clean(){
+    TOREMOVE=""
+
+    for f in $TOREMOVE;do
+        find $G_TMP/ -type f -name "${f}*" -delete
+    done
+    rm -f /dev/shm/sessions_lock
+}
+start(){
+    [ -d "$G_TMP" ]  || install -o $RUN_USER -g $RUN_GROUP -m 770 -d $G_TMP
+    [ -d "$CDCM_LOG_DIR" ]  || install -o $RUN_USER -g $RUN_GROUP -m 770 -d $CDCM_LOG_DIR
+    $START_CMD -p /tmp/cdcm_broker.pid --exec /usr/bin/cdcm_broker >> $CDCM_LOG_DIR/cdcmbroker.log 2>&1
+}
+stop(){
+    if [ $PID -gt 0 ];then
+        kill -9 $PID > /dev/null 2>&1
+    else
+        $KILL_CMD --exec /usr/bin/CDCM_EXE
+    fi
+    exit $?
+}
+status(){
+    if [ $PID -gt 0 ];then
+        if ps -p $PID;then
+            echo "${CDCM_EXE} is running."
+        else
+            echo "${CDCM_EXE} is NOT running."
+        fi
+    else
+        if $STATUS_CMD --exec /usr/bin/$CDCM_EXE;then
+            echo "${CDCM_EXE} is running."
+        else
+            echo "${CDCM_EXE} is NOT running."
+        fi
+    fi
+}
+case $TODO in
+    start)
+        unset LANG
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/cdcm-1.0
+        clean
+        start
+    ;;
+    stop) stop;;
+    status) status;;
+esac
---
+++ src/trustwave/run_cdcm.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# run as root only
+if ( ! id | grep root > /dev/null 2>&1 );
+then
+    echo "You can run this script only as root !!!";
+    exit 1;
+fi
+LAUNCHER=
+while getopts dmcht o
+do      case "$o" in
+        t)      LAUNCHER="strace -ff -o wasp_strace ";;
+        d)      LAUNCHER="cgdb --args";;
+        m)      LAUNCHER="valgrind -v --num-callers=40 --leak-check=full --show-reachable=yes --log-file=cdcm_valgrind_output";;
+        c)      LAUNCHER="valgrind -v --tool=callgrind";;
+        h|[?])    echo -e >&2 "Usage: $0 [-dmh]\n-d\trun with debugger"
+                echo -e >&2 "-m\trun with valgrind"
+                echo -e >&2 "-c\trun with callgrind"
+                echo -e >&2 "-t\trun with strace"
+                echo -e >&2 "-h\tprint this message"
+                exit 1;;
+        esac
+done
+export LOG_OPTIONS
+G_ROOT=/var/cdcm
+G_TMP=${G_ROOT}/run/
+G_LIB_DIR=/usr/lib
+G_BIN_DIR=/usr/bin
+G_LOG_DIR=${G_ROOT}/log
+MD=mkdir
+CP="cp -f"
+RM="rm -f"
+unset_locales()
+{
+    env | grep -E '^LC_|^LANGUAGE' | cut -d '=' -f 1 | while read line;do unset $line;done
+}
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/cdcm-1.0
+ if ! [ -a $procpath ]
+      then
+         echo "process is stopped" >> $filename
+         exit 0
+ fi
+# create TrPManager socket directory
+mkdir -p $G_ROOT
+if  [ -d ${G_LOG_DIR} ]
+  then
+      rm -rf ${G_LOG_DIR}
+fi
+unset LC_CTYPE
+export PATH=/usr/lib:/bin:/usr/bin
+echo core.%p > /proc/sys/kernel/core_pattern
+ulimit -c unlimited
+ulimit -n 131072
+mkdir -p $G_TMP
+unset LANG
+unset_locales
+mount -a
+if [ ! -f /dev/shm/mount.lock ]; then
+    mount /dev/shm
+    touch /dev/shm/mount.lock
+fi
+echo ${LAUNCHER} cdcm_broker
+${LAUNCHER} cdcm_broker
