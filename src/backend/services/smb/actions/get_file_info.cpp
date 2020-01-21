05c59675321834affe9edf2fca61201d6cccc8b6
Assaf Cohen <ascohen@trustwave.com>
2020-01-21 20:04:23 +0200

    changed snakecase for all property names
    Former-commit-id: 3591b4eeaee15620354f94b195a52afea0dd727b

--- src/backend/services/smb/actions/get_file_info.cpp
+++ src/backend/services/smb/actions/get_file_info.cpp
@@ -57,12 +57,14 @@ int SMB_Get_File_Info::act(boost::shared_ptr<session> sess, std::shared_ptr<acti
     pc.extract_info(ret);
     tao::json::events::to_value c;
     c.begin_array();
-    push_back(c, "Size", std::to_string(rc.file_size()));
-    push_back(c, "Path", "FIXME");
-    push_back(c, "LastModified", std::to_string(rc.last_modified()));
+    push_back(c, "size", std::to_string(rc.file_size()));
+    push_back(c, "path", "FIXME");
+    push_back(c, "lastModified", std::to_string(rc.last_modified()));
     std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
     for(const auto& e: ret) {
-        push_back(c, convert.to_bytes(std::u16string(e.first)), convert.to_bytes(std::u16string(e.second)));
+        auto key = convert.to_bytes(std::u16string(e.first));
+        key[0]=std::tolower(key[0]);
+        push_back(c, key, convert.to_bytes(std::u16string(e.second)));
     }
     c.end_array();
 
