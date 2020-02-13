813084efd3803049e5eab64a0cc52c203586ce17
rotem frenkel <rfrenkel@trustwave.com>
2020-02-13 11:48:01 +0200

    removed printing of smbc debug info to console
    Former-commit-id: 2b047bcbe690c2ee051c1b530fa2659807f8eafb

--- src/backend/message_worker.cpp
+++ src/backend/message_worker.cpp
@@ -237,8 +237,7 @@ int message_worker::worker_loop()
         }
         try {
             const tao::json::value v1 = result;
-            //auto reply_body_str = to_string(v1, 2);
-            auto reply_body_str = to_string(v1);
+            auto reply_body_str = to_string(v1,2);
             reply = new zmsg; // will be deleted in recv
             reply->append(reply_body_str.c_str());
         }
