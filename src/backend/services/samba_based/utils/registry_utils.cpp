a1d024c3eabb583051129ad0a98254f72352a7b9
Assaf Cohen <ascohen@trustwave.com>
2020-08-12 20:16:55 +0300

    normalized keys and paths in request with no default hive

--- src/backend/services/samba_based/utils/registry_utils.cpp
+++ src/backend/services/samba_based/utils/registry_utils.cpp
@@ -16,23 +16,24 @@
 #include "registry_utils.hpp"
 #include <boost/algorithm/string.hpp>
 #include <numeric>
-bool trustwave::split_hive_key(const std::string& path, std::string& hivename, std::string& subkeyname)
+void trustwave::split_hive_key(const std::string& path, std::string& hivename, std::string& subkeyname)
 {
+    static constexpr std::string_view slash("\\");
     std::vector<std::string> strs;
-    boost::split(strs,path,boost::is_any_of("\\"));
+    boost::split(strs,path,boost::is_any_of(slash));
     strs.erase(std::remove_if(strs.begin(),
                               strs.end(),
                                     [&](const std::string s)-> bool
                                     { return s.empty(); }),
                strs.end());
     auto slash_fold = [](std::string a, std::string b) {
-        return std::move(a) + '\\' + b;
+        return std::move(a) + slash[0] + b;
     };
     std::string new_path = std::accumulate(std::next(strs.begin()), strs.end(),
                                     strs[0], // start with first element
                                     slash_fold);
 
-    auto pos = new_path.find("\\");
+    auto pos = new_path.find(slash);
     if(std::string::npos != pos) {
         subkeyname.assign(new_path.substr(pos + 1));
         hivename.assign(new_path.substr(0, pos));
@@ -40,20 +41,17 @@ bool trustwave::split_hive_key(const std::string& path, std::string& hivename, s
     else {
         subkeyname.assign(new_path);
     }
-
-    return true;
 }
 bool trustwave::reg_hive_key(const std::string& fullname, uint32_t& reg_type, std::string& subkeyname)
 {
     if(fullname.empty()) { return false; }
     std::string hivename;
     std::string tmp_keyname;
-    bool ret = split_hive_key(fullname, hivename, tmp_keyname);
+    split_hive_key(fullname, hivename, tmp_keyname);
     auto it = hives.find(hivename);
     if(it == hives.end())
     {
-        reg_type = HKEY_LOCAL_MACHINE;
-        subkeyname = fullname;
+        return false;
     }
     else
     {
