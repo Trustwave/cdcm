e49e5c5fea0c5f7393c4bed779172b7253230b7d
Assaf Cohen <ascohen@trustwave.com>
2020-09-15 14:39:32 +0300

    removed test shmem

--- ci_scripts/unittests.bash
+++ ci_scripts/unittests.bash
@@ -5,6 +5,7 @@ cd ../cdcm_testing/tests || exit
 sed -i "s@/var/log/cdcm/testsRunner/@""$PWD""/log/@g" ../functional_tests/testsRunner_settings.json
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/output/lib/:/opt/output/lib/plugins/:$GIT_CLONE_PATH/deps/samba-4.10.6/bin/shared/private/:$GIT_CLONE_PATH/deps/samba-4.10.6/bin/shared
 cd ../../build_cdcm/src/cdcm_testing || exit
+sudo rm -f /dev/shm/*
 ./tests_run --log_sink="$REPORT_FNAME".xml --log_format=JUNIT --log_level=all || code=$?
 if [ $((code)) -eq 0 ]; then echo "success"; else echo 'tests failure'; fi
 java -jar /opt/xunit-to-html/saxon9he.jar -o:"$REPORT_FNAME".html -s:"$REPORT_FNAME".xml -xsl:/opt/xunit-to-html/xunit_to_html.xsl
