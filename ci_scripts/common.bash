7c8e5e0442813321ec64901d870edc5b84d9746a
Assaf Cohen <ascohen@trustwave.com>
2020-09-27 10:53:36 +0300

    added sudo for tee command hopefully will resolve write error

--- ci_scripts/common.bash
+++ ci_scripts/common.bash
@@ -3,5 +3,5 @@ function upload_file {
     SERVER=10.88.240.20
     PORT=2222
     echo uploading "$1" to server "$SERVER"
-    sshpass -f /root/pf scp -P "$PORT" "$1" root@"$SERVER"://opt/tests_results
+    "$2" sshpass -f /root/pf scp -P "$PORT" "$1" root@"$SERVER"://opt/tests_results
 }
