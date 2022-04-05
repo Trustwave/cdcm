9be27e45a81ebe7d14a0042d02882585758a090d
Joel Luellwitz <jluellwitz@trustwave.com>
2022-04-05 13:19:50 -0500

    CRFW-1743 - Added placeholder support for Nexus credentials.

--- ci_scripts/deploy.bash
+++ ci_scripts/deploy.bash
@@ -9,6 +9,8 @@ if [${POM_SNAPSHOT} = '']; then
 fi
 sed -e "s/%{version}/${ARTIFACT_VERSION}/" \
  -e "s/%{rpm_file}/RPMS\/x86_64\/${RPM_FILE}/" pom-template.xml >pom.xml
+sed -i -e "s/%{username}/${NEXUS_USERNAME}/" maven-settings.xml
+sed -i -e "s/%{password}/${NEXUS_PASSWORD}/" maven-settings.xml
 export MVN_CMD="mvn -B -s maven-settings.xml deploy"
 echo "$MVN_CMD"
 $MVN_CMD
