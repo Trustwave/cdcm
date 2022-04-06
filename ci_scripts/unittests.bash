41ef24c17b67c94d2591c8bdc4a7cc12f6ec3b08
Joel Luellwitz <jluellwitz@trustwave.com>
2022-04-06 13:09:01 -0500

    CRFW-1743 - Fixing unittests.bash.

--- ci_scripts/unittests.bash
+++ ci_scripts/unittests.bash
@@ -2,7 +2,7 @@ set -e
 cd build_cdcm/ || exit
 make install
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/output/lib/:/opt/output/lib/plugins/:$GIT_CLONE_PATH/deps/samba-4.10.6/bin/shared/private/:$GIT_CLONE_PATH/deps/samba-4.10.6/bin/shared
-cd ../../build_cdcm/src/cdcm_testing || exit
+cd ../build_cdcm/src/cdcm_testing || exit
 rm -f /dev/shm/*
 ./tests_run --log_sink="$REPORT_FNAME".xml --log_format=JUNIT --log_level=all || code=$?
 if [ $((code)) -eq 0 ]; then echo "success"; else echo 'tests failure'; fi
